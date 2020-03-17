c9f642d8df11e5d662c340f0e96df189cf933689
veezyjay <viktorbasi545@gmail.com>
2020-03-17 20:31:44 +0100

    Fetch data from external api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios'
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <Users />
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true})
+    const res = await axios.get("https://api.github.com/users");
+    console.log(res.data);
+    this.setState({users: res.data, loading: false});
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users users= {this.state.users} loading={this.state.loading} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
