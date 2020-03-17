f84336601f95d36dd4e66122716a9c0abe434cd1
veezyjay <viktorbasi545@gmail.com>
2020-03-17 22:05:08 +0100

    Add clear users functionality

--- src/App.js
+++ src/App.js
@@ -17,12 +17,14 @@ class App extends Component {
     this.setState({users: res.data.items, loading: false});
   }
 
+  clearUsers = () => this.setState({users: [], loading: false});
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false} />
           <Users users= {this.state.users} loading={this.state.loading} />
         </div>
       </div>
