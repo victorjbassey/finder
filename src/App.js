25a4106ab3395c8a7dae53533a344b98b0040519
veezyjay <viktorbasi545@gmail.com>
2020-03-17 19:53:21 +0100

    Refactor to functional components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users'
-import './App.css';
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Users />
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <Users />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
 
-  render() {
+const UserItem = props => {
 
-    const { login, avatar_url, html_url } = this.props.user;
+  const { login, avatar_url, html_url } = props.user;
 
-    return (
-      <div className="card text-center">
-        <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}}/>
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-        </div>
+  return (
+    <div className="card text-center">
+      <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}}/>
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
 }
 
 export default UserItem
