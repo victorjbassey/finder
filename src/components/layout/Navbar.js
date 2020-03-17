a704cbf2a44d350bd16d02ce5a74c10cf056d2c0
veezyjay <viktorbasi545@gmail.com>
2020-03-17 23:21:33 +0100

    Setup react router

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 
 const Navbar = props => {
@@ -9,6 +10,14 @@ const Navbar = props => {
       <h1>
         <i className={props.icon}></i> {props.title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
