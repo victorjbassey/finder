bc4f08f4a0188fa3cba83b146dd1d3f1e3cd72ba
veezyjay <viktorbasi545@gmail.com>
2020-03-18 11:27:26 +0100

    Add not found page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/pages/About";
 import User from "./components/users/User";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import NotFound from "./components/pages/NotFound";
 import "./App.css";
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
