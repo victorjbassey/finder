e554c023c6847e43397041b0ccaa30ca02b29165
veezyjay <viktorbasi545@gmail.com>
2020-03-21 17:39:16 +0100

    Fix responsiveness of users component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -22,7 +22,7 @@ const Users = () => {
 
 const userStyle = {
   display: "grid",
-  gridTemplateColumns: "repeat(3, 1fr)",
+  gridTemplateColumns: "repeat(auto-fit, minmax(12rem, 1fr))",
   gridGap: "1rem"
 };
 
